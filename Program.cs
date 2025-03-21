5b9acf8b73c1cd0d06f771e5768c857be345a61d
Mooneer <mun@Air-User.litsey7>
2025-03-22 01:53:02 +0300

    created class Man

--- Program.cs
+++ Program.cs
@@ -1,22 +1,65 @@
 ﻿using System;
 
 
-class Player
+abstract class Player
 {
-    private static Player player;
-
-    private Player() 
+    public int Speed { get; set; }
+    public int Health { get; set; }
+    public string Name { get; set; }
+    protected Player(string name) 
     {
+        Speed = 10;
+        Health = 100;
+        Name = name;
         Console.WriteLine("Экземпляр Player создан.");
     }
 
-    public static Player create_player()
+    public abstract void hi();
+}
+
+class Man : Player
+{
+    public int Age { get; set; }
+    private string _bio = "";
+    public string Bio {
+        get { return _bio; }
+        private set {_bio = value;}
+    }
+
+    // Конструктор по умолчанию
+    public Man() : base("defaultPlayer") {}
+
+    // Конструктор с параметром
+    public Man(string name) : base(name) {}
+
+    public override void hi()
     {
-        if(player == null)
+        Console.WriteLine("Hi, I'm a man");
+    }
+
+    public void SetBio(string new_bio) {
+        Bio = new_bio;
+        Console.WriteLine("Your bio successfully updated");
+    }
+
+    public void PrintInfo() 
+    {
+        Console.WriteLine($"Name: {Name}, Age: {Age}, Bio: {Bio}");
+    }
+
+    private void Calculate() 
+    {
+        Console.WriteLine("Calculating something...");
+    }
+
+    public static Man CreateDefaultMan()
+    {
+        return new Man
         {
-            player = new Player();
-        }
-            return player;
+            Name = "DefaultMan",
+            Age = 25,
+            Bio = "Just a default man."
+        };
     }
 }
 
@@ -24,6 +67,6 @@ class Program
 {
     public static void Main(string[] args)
     {
-        Player player = Player.create_player();
+        Player player = new Man("Ignat");
     }
 }
