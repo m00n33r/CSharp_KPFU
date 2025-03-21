fb79cd9679dfbda96905181648e5fc7c0c4b92e7
Mooneer <mun@Air-User.litsey7>
2025-03-22 00:57:09 +0300

    created base class

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+
+class Player
+{
+    private static Player player;
+
+    private Player() 
+    {
+        Console.WriteLine("Экземпляр Player создан.");
+    }
+
+    public static Player create_player()
+    {
+        if(player == null)
+        {
+            player = new Player();
+        }
+            return player;
+    }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Player player = Player.create_player();
+    }
+}
